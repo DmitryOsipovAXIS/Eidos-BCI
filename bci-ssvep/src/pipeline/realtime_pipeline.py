38b7cabf11773d6e8f3c8deec89886048bd57250
AxlNuyens <axl.nuyens@student.ucll.be>
2026-05-28 09:44:46 +0200

    added first part of the realtime SSVEP class

---
+++ bci-ssvep/src/pipeline/realtime_pipeline.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import threading
+import time
+from typing import Optional
+
+import numpy as np
+
+from pipeline.pipeline import SSVEPPipeline
+
+class RealtimeCCAPipeline:
+    def __init__(self, stream, frequencies_hz: list[float], sample_rate_hz: float =125.0, window_s: float = 2.0, step_s: float = 1.0, n_harmonics: int = 3, labels: list[str]|None = None, confidence_ratio: float = 1.3, min_absolute: float = 0.02,) -> None:
+        self._stream = stream
+        self._window_samples = int(window_s * sample_rate_hz)
+        self._step_s = step_s
+        self._labels = labels or ["LEFT", "RIGHT"]
+        self._frequencies_hz = frequencies_hz
+        self._confidence_ratio = confidence_ratio
+        self._min_absolute = min_absolute
+
+        self._pipeline = SSVEPPipeline(
+            frequencies_hz=frequencies_hz,
+            sample_rate_hz=sample_rate_hz,
+            n_harmonics=n_harmonics,
+            labels=self._labels
+        )
