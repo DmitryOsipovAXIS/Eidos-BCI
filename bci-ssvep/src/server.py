a40375dad5a10c5698f2f0cf383c44be9adc82d5
MatseVH <49431416+Matse2005@users.noreply.github.com>
2026-06-02 10:52:21 +0200

    Cleaning up the live collecten

--- bci-ssvep/src/server.py
+++ bci-ssvep/src/server.py
@@ -1,12 +1,17 @@
 import asyncio
 from ws.websockets import start, broadcast
 from ws.http import start_http
+from collection.live_collecten import start_live, LiveCollectArgs
 
 
 async def main():
+    loop = asyncio.get_running_loop()
+    args = LiveCollectArgs(no_eeg=True)
+
     await asyncio.gather(
         start(),
-        start_http(broadcast)
+        start_http(broadcast),
+        loop.run_in_executor(None, lambda: start_live(args, broadcast, loop)),
     )
 
 if __name__ == "__main__":
